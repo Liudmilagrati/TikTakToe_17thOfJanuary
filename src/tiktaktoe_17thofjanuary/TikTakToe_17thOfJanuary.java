92daec0651c48dce2e24d4346e5a254fe68bcfb0
Liudmilagrati <120527185+Liudmilagrati@users.noreply.github.com>
2024-02-29 11:29:44 +0000

    Grid setup and display

--- src/tiktaktoe_17thofjanuary/TikTakToe_17thOfJanuary.java
+++ src/tiktaktoe_17thofjanuary/TikTakToe_17thOfJanuary.java
@@ -14,7 +14,35 @@ public class TikTakToe_17thOfJanuary {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        //display grid. We will use an array with strings
+        String [][] gridArray = new String [3][5];
+        gridArray[0][0] = "_";
+        gridArray[0][1] = "|";
+        gridArray[0][2] = "_";
+        gridArray[0][3] = "|";
+        gridArray[0][4] = "_";
+        
+        gridArray[1][0] = "_";
+        gridArray[1][1] = "|";
+        gridArray[1][2] = "_";
+        gridArray[1][3] = "|";
+        gridArray[1][4] = "_";
+        
+        gridArray[2][0] = " ";
+        gridArray[2][1] = "|";
+        gridArray[2][2] = " ";
+        gridArray[2][3] = "|";
+        gridArray[2][4] = " ";
+        
+        for (int row=0; row<gridArray.length; row++) {
+            
+            for (int col=0; col<gridArray[row].length; col++){
+                System.out.print(gridArray[row][col]);
+            }
+            System.out.println(""); // to have an emty line between rows 
+        }
+        
     }
     
 }
