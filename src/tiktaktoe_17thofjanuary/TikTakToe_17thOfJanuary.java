f5087d42cdd7b942da3f41c390de3fe42837015f
Liudmilagrati <120527185+Liudmilagrati@users.noreply.github.com>
2024-02-29 22:56:29 +0000

    Refactored into Grid class

--- src/tiktaktoe_17thofjanuary/TikTakToe_17thOfJanuary.java
+++ src/tiktaktoe_17thofjanuary/TikTakToe_17thOfJanuary.java
@@ -10,64 +10,7 @@ package tiktaktoe_17thofjanuary;
  */
 public class TikTakToe_17thOfJanuary {
     
-    public static String[][]setupGridArray(){
-       
-        //initiliazing and display the grid. We will use an array with strings
-        String [][] gridArray = new String [3][5];
-        gridArray[0][0] = "_";
-        gridArray[0][1] = "|";
-        gridArray[0][2] = "_";
-        gridArray[0][3] = "|";
-        gridArray[0][4] = "_";
-        
-        gridArray[1][0] = "_";
-        gridArray[1][1] = "|";
-        gridArray[1][2] = "_";
-        gridArray[1][3] = "|";
-        gridArray[1][4] = "_";
-        
-        gridArray[2][0] = " ";
-        gridArray[2][1] = "|";
-        gridArray[2][2] = " ";
-        gridArray[2][3] = "|";
-        gridArray[2][4] = " ";
-        
-        return gridArray;  
-    }
-    
-    //method to display the grid
-    public static void  displayGrid(String[][] gridArray) {
-        for (int row=0; row<gridArray.length; row++) {
-            
-            for (int col=0; col<gridArray[row].length; col++){
-                System.out.print(gridArray[row][col]);
-            }
-            System.out.println(""); // to have an emty line between rows 
-        }     
-    }
-
-    // method to update the grid 
-    // we will need gridArray- grid , int[][] position- whats been uodated , String letter - whats nbeen updated too 
-    //for convinice position will refer 3 by 3, how the user will conceptuliase it
-
-    public static void updateGrid(String[][] gridArray, int[] position, String letter) {
-       
-        //Position 3x3 and needs the player needs 
-        //position = {2, 0}
-        gridArray[position[0]][2*position[1]] = letter; // only even cols - use 2*n+1 for only add columns
-        /*
-        position 
-        0 1 2 
-        0 1 2 
-        0 1 2 
-        
-        grid
-        x|x|x - we want to skip every second element | that's why we multiply by 2
-        01234 - positions
-        
-        */
-    }
-    
+ 
     
     /**
      * @param args the command line arguments
@@ -79,14 +22,16 @@ public class TikTakToe_17thOfJanuary {
         // call function on grid array method to display teh grid 
          displayGrid(gridArray); 
          
-         //call update grid method  and declare it as new int array
+         //call update grid method  and declare it as new int array and display teh updates 
          updateGrid(gridArray, new int[] {0, 0}, "X");
          displayGrid(gridArray); 
          System.out.println("");
          updateGrid(gridArray, new int[] {1, 2}, "O");
          displayGrid(gridArray); 
+         
+         
         
-        
-    }
-    
+       
+         
+    } 
 }
